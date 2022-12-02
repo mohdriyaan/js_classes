b3c70f1a9c411643a5897d90fb0095795dea756f
mrrayaan123 <mohammedrayaan1@gmail.com>
2022-12-02 21:38:23 +0530

    Day 01 Files added

---
+++ Day 01/variables.js
@@ -0,0 +1,29 @@
+//Variables are used to store data and value
+
+/*There are 3 types of variables:
+  1.var
+  2.let
+  3.const 
+*/
+
+var box = 10 //var can resign and redeclare.//IN var redeclaring happens,which increases memory.
+console.log(box)
+
+var box1 = 20
+console.log(box1)
+
+var box3= "Hello"
+console.log(box3)
+
+var box4 = 50
+var box4= 80
+console.log(box4)
+
+let box5=100 //let can only reassign.//In let we update value.
+box5 =120
+console.log(box5)
+
+//If we declare box without variable then by default it will declare var
+
+const box = 90
+//Here the value is constant and cannot be redefined or updated.
