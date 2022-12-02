6d1d7580077f44b91bc03c107eb961010eb15ea6
mrrayaan123 <mohammedrayaan1@gmail.com>
2022-12-03 03:11:04 +0530

    Files Added

---
+++ Day 01/immutable.js
@@ -0,0 +1,8 @@
+// Immutable - Primitive Data Type
+
+let a1 = "Suhail"
+
+a1[0] = "R"
+a1[1] = "O"
+a1 = "Roushan"
+console.log(a1);
---
+++ Day 01/mutability.js
@@ -0,0 +1,20 @@
+// Mutable - Non Primitive Data Types
+// Objects and ARRAYS
+
+obj = {
+    name: "Amina",
+    age: 20,
+  };
+  
+  obj.name = "Namira";
+  // console.log(obj);
+  
+  let arr = ["A", "M", "i", 20];
+  ///        0   1   2   3
+  
+  arr[0] = "S"
+  arr[1] = "u"
+  arr[2] = "h"
+  arr[3] = 10;
+  console.log(arr);
+  
