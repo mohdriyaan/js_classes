ae4940c92e194109b839d8aa02ffc413ab647125
mrrayaan123 <mohammedrayaan1@gmail.com>
2022-12-03 02:58:00 +0530

    Files Added

---
+++ Day 03/type_conversion.js
@@ -0,0 +1,20 @@
+//Type Conversions
+//changing from one data type to another
+
+let a1 = "String"
+let a2 = 100
+let a3 = true
+let a4 = null
+let a5 = undefined
+
+//String Conversion
+let temp1 = String(a2)
+console.log(a2)
+console.log(typeof a2)
+
+//Number Conversion
+let temp2 = Number(a1)
+console.log(a1)
+console.log(typeof a1)
+
+//Null Conversion
