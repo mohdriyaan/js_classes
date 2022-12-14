b8d4ac3f613193c4f85e645cb7d78ad0841f4e2c
mrrayaan123 <mohammedrayaan1@gmail.com>
2022-12-14 22:35:58 +0530

    Files Added

---
+++ Day 09/string_methods/includes.js
@@ -0,0 +1,6 @@
+// includes() checks whether the sub string exists in the string stored in variable
+let a = "Welcome To Javascript"
+console.log(a.includes("Welcome"))
+console.log(a.includes("to")) // JS is case sensitive so t and T are not same.
+console.log(a.includes(" "))
+
