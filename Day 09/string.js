8ac0abe5fb4af03d96d4ccacfb579f3148793b15
mrrayaan123 <mohammedrayaan1@gmail.com>
2022-12-14 21:24:43 +0530

    Files Added

---
+++ Day 09/string.js
@@ -0,0 +1,16 @@
+// Strings
+/*
+    >Set of characters // Means random characters
+    >Sequence of characters // Means characters in order
+    >Array of characters
+    >Primitive Data Type
+*/
+
+var name="hello"
+console.log(name)
+var name1="cool"
+name1[0]="l"
+console.log(name1) // Here "c" will not be changed because string is immutable 
+// Here a single value cannot be manipulated and the whole value needs to be updated.
+
+
