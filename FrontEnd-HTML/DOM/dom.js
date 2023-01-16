2d1389315aca36dd6c0145708ed9f7f3955f7407
mrrayaan123 <mohammedrayaan1@gmail.com>
2023-01-16 15:06:19 +0530

    Files Added

---
+++ FrontEnd-HTML/DOM/dom.js
@@ -0,0 +1,11 @@
+// console.dir(document);
+console.log(document.title);
+console.log(document.domain)
+console.log(document.URL)
+console.log(document.head)
+console.log(document.body)
+console.log(document.all)
+document.all[5].textContent="hello123";
+console.log(document.title)
+console.log(document.getElementsByTagName("form"));
+
